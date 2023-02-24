dd22549edf8f040c1882d2b9d23e54cc34ee816d
Christopher Carrasco <christopher3carrasco@gmail.com>
2023-02-23 20:39:13 -0500

    feat: add name symbol interface

---
+++ src/com/carrasco/util/NameSymbol.java
@@ -0,0 +1,9 @@
+package com.carrasco.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public interface NameSymbol {
+    List<String> currencyName = Arrays.asList("Dólares", "Euros", "Libras esterlinas", "Yen japonés", "Won surcoreano");
+    List<String> currencySymbol = Arrays.asList("USD", "EUR", "GBP", "JPY", "KRW");
+}
