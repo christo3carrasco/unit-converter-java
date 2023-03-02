8da183f5242efa238f2d12d620c77e10bc4ed9aa
Christopher Carrasco <christopher3carrasco@gmail.com>
2023-03-02 12:57:04 -0500

    feat: add data combo interface

---
+++ src/com/carrasco/util/DataCombo.java
@@ -0,0 +1,9 @@
+package com.carrasco.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public interface DataCombo {
+    List<String> unitsCombo = Arrays.asList("Divisas", "Superficie", "Longitud", "Temperatura", "Volumen", "Masa", "Datos");
+    List<String> currencyCombo = Arrays.asList("PEN ➜ USD", "PEN ➜ EUR", "PEN ➜ GBP", "PEN ➜ JPY", "PEN ➜ KRW", "USD ➜ PEN", "EUR ➜ PEN", "GBP ➜ PEN", "JPY ➜ PEN", "KRW ➜ PEN");
+}
